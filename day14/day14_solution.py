a47b30145e073ad34bf959c423712edc51c42209
Kate Williams <kate.williams109@nhs.net>
2024-12-15 00:22:35 +0000

    part 2 of this was super fun!!

--- day14/day14_solution.py
+++ day14/day14_solution.py
@@ -1,26 +1,37 @@
 import re
 import numpy as np
 
-def read_in_robots(input_file: str) -> list[dict[str:tuple[int]]]:
-    digit_regex = r'(-?\d+),(-?\d+)' 
+
+def read_in_robots(input_file: str) -> list[dict[str : tuple[int]]]:
+    digit_regex = r"(-?\d+),(-?\d+)"
     all_robots = []
     with open(input_file) as raw_robots:
         for robot in raw_robots.readlines():
             position_and_velocity = {}
             raw_position_and_velocity = re.findall(digit_regex, robot)
-            position_and_velocity['position'] = tuple(map(int, raw_position_and_velocity[0]))
-            position_and_velocity['velocity'] = tuple(map(int, raw_position_and_velocity[1]))
+            position_and_velocity["position"] = tuple(
+                map(int, raw_position_and_velocity[0])
+            )
+            position_and_velocity["velocity"] = tuple(
+                map(int, raw_position_and_velocity[1])
+            )
             all_robots.append(position_and_velocity)
     return all_robots
 
-def get_final_position(robot: dict[str:tuple[int]], width: int, height: int, seconds: int) -> tuple[int]:
+
+def get_final_position(
+    robot: dict[str : tuple[int]], width: int, height: int, seconds: int
+) -> tuple[int]:
     x_start, y_start = robot["position"]
     x_velocity, y_velocity = robot["velocity"]
-    final_x = (x_start + (x_velocity * seconds)) % width 
+    final_x = (x_start + (x_velocity * seconds)) % width
     final_y = (y_start + (y_velocity * seconds)) % height
     return final_x, final_y
 
-def get_quadrant(final_x: int, final_y: int, middle_x: int, middle_y: int) -> int|None:
+
+def get_quadrant(
+    final_x: int, final_y: int, middle_x: int, middle_y: int
+) -> int | None:
     if final_x < middle_x:
         if final_y < middle_y:
             return 1
@@ -33,11 +44,14 @@ def get_quadrant(final_x: int, final_y: int, middle_x: int, middle_y: int) -> in
             return 4
     return None
 
-def calculate_safety_factor_p1(input_file: str, width: int = 101, height: int = 103, seconds: int = 100) -> int:
+
+def calculate_safety_factor_p1(
+    input_file: str, width: int = 101, height: int = 103, seconds: int = 100
+) -> int:
     robots = read_in_robots(input_file)
     middle_x = width // 2
     middle_y = height // 2
-    quadrants = {1:0, 2:0, 3:0, 4:0}
+    quadrants = {1: 0, 2: 0, 3: 0, 4: 0}
     for robot in robots:
         final_x, final_y = get_final_position(robot, width, height, seconds)
         if (quadrant := get_quadrant(final_x, final_y, middle_x, middle_y)) is not None:
@@ -47,31 +61,37 @@ def calculate_safety_factor_p1(input_file: str, width: int = 101, height: int = 
         safety_factor *= robot_count
     return safety_factor
 
-def plot_grid_for_one_time(robots: dict[str:tuple[int]], seconds: int, width: int = 101, height: int = 103) -> np.ndarray:
-    grid = np.full((height, width), ' ')
+
+def plot_grid_for_one_time(
+    robots: dict[str : tuple[int]], seconds: int, width: int = 101, height: int = 103
+) -> np.ndarray:
+    grid = np.full((height, width), " ")
     for robot in robots:
         x, y = get_final_position(robot, width, height, seconds)
-        grid[y,x] = '#'
+        grid[y, x] = "#"
     return grid
+
 
 def find_christmas_tree(input_file: str, max_seconds: int = 10_000):
     robots = read_in_robots(input_file)
     for seconds in range(max_seconds):
         current_grid = plot_grid_for_one_time(robots, seconds)
-        grid_as_string = [''.join(line) for line in current_grid]
+        grid_as_string = ["".join(line) for line in current_grid]
         with open("day14/find_a_tree.txt", "a") as tree_file:
-            tree_file.write('--------------------\n')
-            tree_file.write(f'{seconds=}')
+            tree_file.write("--------------------\n")
+            tree_file.write(f"{seconds=}")
             for line in grid_as_string:
-                tree_file.write(f'{line}\n')
-            tree_file.write('--------------------\n')
+                tree_file.write(f"{line}\n")
+            tree_file.write("--------------------\n")
+
 
 def print_found_tree(input_file: str):
     robots = read_in_robots(input_file)
     current_grid = plot_grid_for_one_time(robots, 6475)
-    grid_as_string = [''.join(line) for line in current_grid]
+    grid_as_string = ["".join(line) for line in current_grid]
     for line in grid_as_string:
         print(line)
+
 
 if __name__ == "__main__":
     print(calculate_safety_factor_p1("day14/day14_final_input.txt"))
